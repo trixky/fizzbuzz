5fb9d06dca01ea96cd61cbb4ee7b127a5d53928d
trixky <mathisbois.dev@gmail.com>
2021-07-21 10:19:21 +0200

    optimize fizzbuzz_generator

--- server/logic/logic.fizzbuzz.go
+++ server/logic/logic.fizzbuzz.go
@@ -14,29 +14,21 @@ type Data_fizzbuzz struct {
 // fizzbuzz_generator generates the fizzbuzz from user inputs.
 func Fizzbuzz_generator(extracted_fizzbuzz *extractors.Fizzbuzz) (data_fizzbuzz Data_fizzbuzz) {
 	data_fizzbuzz.Fizzbuzz = make([]string, extracted_fizzbuzz.Limit)
-	str1str2 := extracted_fizzbuzz.Str1 + extracted_fizzbuzz.Str2
 
 	for i := 0; i < extracted_fizzbuzz.Limit; i++ {
 		ii := i + 1
-		fizzbuzzed := false
 
 		if ii%extracted_fizzbuzz.Int1 == 0 {
-			// If it's a multiple of int1.
-			fizzbuzzed = true
-		}
-		if ii%extracted_fizzbuzz.Int2 == 0 {
-			// If it's a multiple of int2.
-			if fizzbuzzed {
-				// If it's a multiple of int1 and int2.
-				data_fizzbuzz.Fizzbuzz[i] = str1str2
+			if ii%extracted_fizzbuzz.Int2 == 0 {
+				// If it's a multiple of int1.
+				data_fizzbuzz.Fizzbuzz[i] = extracted_fizzbuzz.Str1 + extracted_fizzbuzz.Str2
 			} else {
-				// If it's only a multiple of int2.
-				fizzbuzzed = true
-				data_fizzbuzz.Fizzbuzz[i] = extracted_fizzbuzz.Str2
+				// If it's a multiple of int1 and int2.
+				data_fizzbuzz.Fizzbuzz[i] = extracted_fizzbuzz.Str1
 			}
-		} else if fizzbuzzed {
-			// If it's only a multiple of int1.
-			data_fizzbuzz.Fizzbuzz[i] = extracted_fizzbuzz.Str1
+		} else if ii%extracted_fizzbuzz.Int2 == 0 { // repeating code but more optimized
+			// If it's a multiple of int2.
+			data_fizzbuzz.Fizzbuzz[i] = extracted_fizzbuzz.Str2
 		} else {
 			// If it's not multiple of int1 or int2.
 			data_fizzbuzz.Fizzbuzz[i] = strconv.Itoa(ii)
